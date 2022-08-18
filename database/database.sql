eddcf30dc1dfaaafe97416fc0e81c93f1f184261
ING Jeferson Vergara <jf.vergara12@gmail.com>
2022-08-18 13:26:41 -0500

    readme

--- database/database.sql
+++ database/database.sql
@@ -6,12 +6,40 @@ CREATE TABLE user(
     email TEXT
 );
 
+
+
 INSERT INTO  user (name,email) VALUES ('admin','user@gmail.com'),('juan','juan@gmail.com');
 
+-- public.departamento definition
+
+-- Drop table
+
+-- DROP TABLE public.departamento;
 
 CREATE TABLE public.departamento (
-	codigo integer NOT NULL,
+	codigo int4 NOT NULL,
 	nombre varchar NOT NULL,
-	presupuesto double precision NOT NULL,
+	presupuesto float8 NOT NULL,
 	CONSTRAINT departamento_pk PRIMARY KEY (codigo)
 );
+
+-- public.empleado definition
+
+-- Drop table
+
+-- DROP TABLE public.empleado;
+
+CREATE TABLE public.empleado (
+	codigo int4 NOT NULL,
+	nif varchar NOT NULL,
+	nombre varchar NOT NULL,
+	apellido1 varchar NOT NULL,
+	apellido2 varchar NULL,
+	codigo_departamento int4 NOT NULL,
+	CONSTRAINT empleado_pk PRIMARY KEY (codigo)
+);
+
+
+-- public.empleado foreign keys
+
+ALTER TABLE public.empleado ADD CONSTRAINT empleado_fk FOREIGN KEY (codigo_departamento) REFERENCES public.departamento(codigo) ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ readme.md
@@ -0,0 +1,44 @@
+# Servidor
+
+#### Runserver
+http://localhost:1234
+```
+npm run dev
+```
+
+
+# Empleado
+## Obtener lista de empleado
+get /api/empleado
+
+## Obtener un empleado
+get /api/empleado/:id
+
+## Eliminar un empleado
+delete /api/empleado/delete/:id
+
+## Actualizar un empleado
+put /api/empleado/update/:id 
+
+## Crear un empleado
+post /api/empleado/create
+
+
+# departamento
+
+# Obtener lista de departamento
+get /api/departamento
+
+# Obtener un departamento
+get /api/departamento/:id 
+
+# Eliminar un departamento
+delete /api/departamento/delete/:id 
+
+# Actualizar un departamento
+put /api/departamento/update/:id
+
+# Crear un departamento
+post /api/departamento/create
+
+
